be19e4e3071b6c29eaf159d0f5c8a8eb1aec565a
sachalips <sacha.alips@gmail.com>
2026-05-30 13:40:59 +0200

    Update SmoothScrollProvider.tsx

--- src/components/SmoothScrollProvider.tsx
+++ src/components/SmoothScrollProvider.tsx
@@ -4,6 +4,12 @@ import Lenis from 'lenis'
 
 export default function SmoothScrollProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
+    const isSafari =
+      typeof window !== 'undefined' &&
+      /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+
+    if (isSafari) return
+
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
